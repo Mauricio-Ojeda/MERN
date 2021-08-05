28d169172067f705052665357dd9d459384b6514
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-08-05 08:44:44 -0300

    creado form agregar paciente

--- src/components/administrar-pacientes/AgregarPacientes.js
+++ src/components/administrar-pacientes/AgregarPacientes.js
@@ -1,5 +1,6 @@
 import React, {Fragment} from 'react'
 
+import { Row  } from 'react-bootstrap';
 import {Form, Button } from 'react-bootstrap'
 
 const AgregarPacientes = () => {
@@ -7,26 +8,47 @@ const AgregarPacientes = () => {
         <Fragment>
              <Button variant="primary" className="btn-block font-weight-bold "> Agregar Paciente</Button>
 
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+            <Form className="text-left">
+                <Form.Group className="mb-4" controlId="formBasicEmail">
+                    <Form.Label>Nombre Propietario</Form.Label>
+                    <Form.Control type="text" size="lg" placeholder="Nombre Propietario" />
                     <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
+                <Row className="mx-auto">
+                    <Form.Group className="mb-4 mr-2" controlId="formBasicEmail">
+                        <Form.Label>Nombre Mascota</Form.Label>
+                        <Form.Control type="text" size="lg" placeholder="Nombre Mascota" />                    
+                    </Form.Group>
+                    <Form.Group className="mb-4 mr-2" controlId="formBasicEmail">
+                        <Form.Label>Especie</Form.Label>
+                        <Form.Control type="text" size="lg" placeholder="Especie" />                    
+                    </Form.Group>
+                    <Form.Group className="mb-4" controlId="formBasicEmail">
+                        <Form.Label>Edad</Form.Label>
+                        <Form.Control type="number" size="lg" placeholder="Edad" />                    
+                    </Form.Group>
+                </Row>
+                <Form.Group className="mb-4" controlId="formBasicEmail">
+                        <Form.Label>Sintomas</Form.Label>
+                        <Form.Control 
+                            as="textarea"  
+                            placeholder="Sintomas"
+                            size="lg"
+                            style= {{height: '100px'}}                        
+                         />                    
+                    </Form.Group>
                 <Form.Group controlId="formFileMultiple" className="mb-3">
                     <Form.Label>Agregar Estudio</Form.Label>
-                    <Form.Control type="file" multiple />
+                    <Form.Control  type="file" multiple />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                <Form.Group className="mb-4" controlId="formBasicPassword">
+                    <Form.Label>Contacto</Form.Label>
+                    <Form.Control size="lg" type="text" placeholder="Contacto" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+               
                 <Button variant="primary" type="submit" className="font-weight-bold btn-block">
                     Agregar
                 </Button>
